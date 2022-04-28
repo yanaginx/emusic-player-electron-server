ac9dad861044e046351e09d88338db673fb126ea
Duong Van Chan <duongchan159@gmail.com>
2022-04-28 08:37:13 +0700

    Fix the hand gesture on recognition function

--- server/controllers/handGestureController.js
+++ server/controllers/handGestureController.js
@@ -1,15 +1,22 @@
 const asyncHandler = require("express-async-handler");
 const { spawn } = require("child_process");
+const os = require("os");
 let handGesture = null;
 
 // @desc    Enable hand gesture mode
 // @route   GET /api/hand_gesture/enable
 // @access  Public
 const enableHandGesture = asyncHandler(async (req, res) => {
-  handGesture = spawn("python3", ["./server/python_scripts/hand_gesture.py"]);
+  handGesture =
+    os.platform() === "win32"
+      ? spawn("python", ["./server/python_scripts/hand_gesture_window.py"])
+      : spawn("python3", ["./server/python_scripts/hand_gesture_linux.py"]);
   handGesture.on("spawn", () => {
     console.log("Hand Gesture Mode Enabled");
     res.status(200).json({ message: "Hand gesture open successfully" });
+  });
+  handGesture.on("data", (data) => {
+    console.log(data.toString());
   });
   handGesture.on("error", (err) => {
     res.status(500).json({
--- server/python_scripts/hand_gesture_linux.py
+++ server/python_scripts/hand_gesture_linux.py
@@ -1,12 +1,8 @@
 import cv2
-import mediapipe as mp
 import numpy as np
 import time
-import math
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import pyautogui
+import alsaaudio
 
 import HandTrackingModule as htm
 
@@ -17,22 +13,32 @@ cTime = 0
 
 detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-volumeRange = volume.GetVolumeRange()
+# north west corner of the screen: x=288, y=172
+# (play/pause): x=625, y=725
+# (next track): x=570, y=725
+# (previous track): x=675, y=725
+# (end of volume bar): x=1295, y=725
+# (begin of volume bar): x=1225, y=725
+# TODO: Correct this values if there is any mistake
+xPlay = 625 - 288
+xNext = 570 - 288
+xPrev = 675 - 288
+xBegin = 1295 - 288
+xEnd = 1225 - 288
+yPlay = yNext = yPrev = yEnd = yBegin = 725 - 172
+# x for linux = windows_x - 288
+# y for linux = windows_y - 172
 
 # Uncomment if u want to set the default system volume
 # Range -65 (0%) to 0 (100%)
-volume.SetMasterVolumeLevel(0, None)
 
-minVolume = volumeRange[0]
-maxVolume = volumeRange[1]
+m = alsaaudio.Mixer()
 
 # Hand range
 minHandRange = 50
 maxHandRange = 150
 
+# Volume variable set up
 vol = 0
 volBar = 400
 volPercent = 100
@@ -115,14 +121,10 @@ while True:
 
     if len(lmList) != 0:
 
-        # print(hand_label)
-
         # STEP 1: Filter based on size
         area = (bbox[2] * bbox[3]) // 100
 
         if 150 < area < 1000:
-
-            # print(hand_label)
 
             # STEP 2: Find distance between index and thumb
             length, img, lineInfo = detector.findDistance(4, 8, img)
@@ -140,14 +142,16 @@ while True:
 
             # STEP 5: Check finger up
             fingers = detector.fingersUp()
-            # print(fingers)
+            print(fingers)
 
             # STEP 5.1: If pinky down then set the volume
             if isSetVolume(fingers, hand_label):
                 cntFingerDownFrame += 1
                 if cntFingerDownFrame >= 20:
-                    volume.SetMasterVolumeLevelScalar(volPercent / 100, None)
-                    cv2.circle(img, lineInfo[2], 10, (0, 255, 0), cv2.FILLED)
+                    # m.setvolume(volPercent)
+                    distance = volPercent / 100 * (xEnd - xBegin)
+                    pyautogui.click(x=xEnd - distance, y=yBegin)
+                    # cv2.circle(img, lineInfo[2], 10, (0, 255, 0), cv2.FILLED)
                     cntFingerDownFrame = 0
             else:
                 cntFingerDownFrame = 0
@@ -155,10 +159,8 @@ while True:
             if isPausePlayMode(fingers, hand_label):
                 cntPausePlayModeFrame += 1
                 if cntPausePlayModeFrame >= 50:
-                    # (play/pause): x=492, y=848
-
                     # pyautogui.press('playpause')
-                    pyautogui.click(x=492, y=848)
+                    pyautogui.click(xPlay, yPlay)
                     cntPausePlayModeFrame = 0
             else:
                 cntPausePlayModeFrame = 0
@@ -166,9 +168,8 @@ while True:
             if isPreviousTrack(fingers, hand_label):
                 cntPrevTrackFrame += 1
                 if cntPrevTrackFrame >= 50:
-                    # (previous track): x=445, y=848
                     # pyautogui.press('prevtrack')
-                    pyautogui.click(x=445, y=848)
+                    pyautogui.click(xPrev, yPrev)
                     cntPrevTrackFrame = 0
             else:
                 cntPrevTrackFrame = 0
@@ -176,9 +177,8 @@ while True:
             if isNextTrack(fingers, hand_label):
                 cntNextTrackFrame += 1
                 if cntNextTrackFrame >= 50:
-                    # (next track): x=548, y=848
                     # pyautogui.press('nexttrack')
-                    pyautogui.click(x=548, y=848)
+                    pyautogui.click(xNext, yNext)
                     cntNextTrackFrame = 0
             else:
                 cntNextTrackFrame = 0
@@ -186,21 +186,19 @@ while True:
     # # Drawing
     # cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     # cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    # cv2.putText(img, f'{int(volPercent)} %', (48, 438),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-
-    # cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
-    # cv2.putText(img, 'VolSet: ' + str(int(cVol)), (300, 70),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
-    # Frame rate
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-
-    # cv2.putText(img, 'FPS: ' + str(int(fps)), (10, 70),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
+    # cv2.putText(img, f'{int(volPercent)} %', (48, 438), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+    #
+    # cVol = int(m.getvolume()[0])
+    # cv2.putText(img, 'VolSet: ' + str(int(cVol)), (300, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    #
+    # # Frame rate
+    # cTime = time.time()
+    # fps = 1 / (cTime - pTime)
+    # pTime = cTime
+    #
+    # cv2.putText(img, 'FPS: ' + str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    #
     # cv2.imshow('image', img)
+
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
